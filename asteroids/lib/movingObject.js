b911d0a7efeee0f35970a7581c3b4bc0c626b350
Tomas Batalla <tebatalla@gmail.com>
2015-03-23 13:26:27 -0700

    Moving object class

---
+++ asteroids/lib/movingObject.js
@@ -0,0 +1,31 @@
+(function() {
+  window.A = A || {};
+
+  A.MovingObject = function(opts) {
+    this.pos = opts.pos;
+    this.vel = opts.vel;
+    this.radius = opts.radius;
+    this.color = opts.color;
+    this.arc = opts.arc || (Math.PI * 2);
+  };
+
+  A.MovingObject.prototype = {
+    draw: function(ctx) {
+      var startAngle = Math.atan(this.vel[1]/this.vel[0]) + Math.PI + (this.arc / 2);
+      ctx.fillStyle = this.color;
+      ctx.beginPath();
+      ctx.arc(this.pos[0],
+        this.pos[1],
+        this.radius,
+        startAngle,
+        startAngle + this.arc,
+        false);
+      ctx.fill();
+    },
+    move: function() {
+      this.pos[0] += this.vel[0];
+      this.pos[1] += this.vel[1];
+    }
+  };
+
+})();
