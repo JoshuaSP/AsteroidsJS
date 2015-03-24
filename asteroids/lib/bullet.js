819e67d24080990dc23b36f6fccac97ccad3a8e2
Tomas Batalla <tebatalla@gmail.com>
2015-03-23 18:01:20 -0700

    Bullets to not wrap

--- asteroids/lib/bullet.js
+++ asteroids/lib/bullet.js
@@ -29,4 +29,13 @@
       this.game.remove(this);
     }
   }
+
+  A.Bullet.prototype.move = function() {
+    this.pos[0] += this.vel[0];
+    this.pos[1] += this.vel[1];
+    if (this.pos[0] < 0 || this.pos[1] < 0 ||
+        this.pos[0] > this.game.dimX || this.pos[1] > this.game.dimY ) {
+      this.game.remove(this);
+    }
+  };
 }();
